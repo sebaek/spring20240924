0cdb43f408ed14ed752597b5f4ab7ebaa13741aa
sebaek <sebaek@gmail.com>
2024-09-24 16:31:31 +0900

    request param

---
+++ src/main/java/com/example/spring20240924/controller/Controller03.java
@@ -0,0 +1,63 @@
+package com.example.spring20240924.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.WebRequest;
+
+@Controller
+public class Controller03 {
+
+    // /main3/sub1?name=son
+    @RequestMapping("/main3/sub1")
+    public void method1(@RequestParam(name = "name") String param) {
+        System.out.println("name = " + param);
+        System.out.println("Controller03.method1");
+    }
+
+    // /main3/sub2?address=seoul
+    @RequestMapping("/main3/sub2")
+    public void method2(@RequestParam(name = "address") String param) {
+        System.out.println("param = " + param);
+    }
+
+    // /main3/sub3?address=seoul
+    @RequestMapping("/main3/sub3")
+    public void method3(@RequestParam(value = "address") String param) {
+        System.out.println("param = " + param);
+    }
+
+    // /main3/sub4?address=seoul
+    @RequestMapping("/main3/sub4")
+    public void method4(@RequestParam("address") String param) {
+        System.out.println("param = " + param);
+    }
+
+    // /main3/sub5?email=gmail&location=us
+    @RequestMapping("/main3/sub5")
+    public void method5(@RequestParam("email") String param1,
+                        @RequestParam("location") String param2) {
+        System.out.println("param1 = " + param1);
+        System.out.println("param2 = " + param2);
+    }
+
+
+    // /main3/sub6?email=yahoo
+    @RequestMapping("/main3/sub6")
+    public void method6(String email) {
+        System.out.println("param1 = " + email);
+    }
+
+    // /main3/sub7?address=seoul
+    //  : seoul 출력
+    //  @RequestParam 어노테이션과 value 속성의 값 명시
+
+    // /main3/sub8?address=seoul
+    //  : seoul 출력
+    // @RequestParam 어노테이션 명시 (단, value 속성값 생략)
+
+    // /main3/sub9?address=seoul
+    //  : seoul 출력
+    // @RequestParam 어노테이션 생략
+
+}
