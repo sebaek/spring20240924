54740bf6099223f63b4204879d511cdff4a599fe
sebaek <sebaek@gmail.com>
2024-09-24 14:57:46 +0900

    parameter

--- src/main/java/com/example/spring20240924/controller/Controller02.java
+++ src/main/java/com/example/spring20240924/controller/Controller02.java
@@ -1,4 +1,17 @@
 package com.example.spring20240924.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.WebRequest;
+
+@Controller
 public class Controller02 {
+
+    // localhost:8080/main2/sub1
+    @RequestMapping("/main2/sub1")
+    public void sub1(WebRequest request){
+        String name = request.getParameter("name");
+        System.out.println("name = " + name);
+        System.out.println("Controller02.sub1");
+    }
 }
