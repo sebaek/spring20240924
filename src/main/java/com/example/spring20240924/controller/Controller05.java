b0b8c5674a1753704b933afff16e3b94fd949867
sebaek <sebaek@gmail.com>
2024-09-25 09:50:56 +0900

    request param

--- src/main/java/com/example/spring20240924/controller/Controller05.java
+++ src/main/java/com/example/spring20240924/controller/Controller05.java
@@ -1,4 +1,53 @@
 package com.example.spring20240924.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("main5")
 public class Controller05 {
+
+    // ?name=son&address=seoul
+    @RequestMapping("sub1")
+    public void sub1(@RequestParam("name") String p1,
+                     @RequestParam("address") String p2) {
+        System.out.println("p1 = " + p1);
+        System.out.println("p2 = " + p2);
+    }
+
+    // /main5/sub2?name=명보&address=국대
+    @RequestMapping("sub2")
+    public void sub2(@RequestParam String name,
+                     @RequestParam String address) {
+        System.out.println("name = " + name);
+        System.out.println("address = " + address);
+    }
+
+    // /main5/sub3?name=명보&address=국대
+    @RequestMapping("sub3")
+    public void sub3(String name,
+                     String address) {
+        System.out.println("name = " + name);
+        System.out.println("address = " + address);
+    }
+
+    // /main5/sub4?name=희찬
+    @RequestMapping("sub4")
+    public void sub4(@RequestParam(required = false) String name,
+                     @RequestParam(required = false) String address) {
+        System.out.println("name = " + name);
+        System.out.println("address = " + address);
+    }
+
+    // /main5/sub5?name=흥민&address=런던
+    // /main5/sub5?address=제주
+    // /main5/sub5?name=강인
+    // /main5/sub5
+    @RequestMapping("sub5")
+    public void sub5(@RequestParam(defaultValue = "게스트") String name,
+                     @RequestParam(defaultValue = "서울역") String address) {
+        System.out.println("name = " + name);
+        System.out.println("address = " + address);
+    }
 }
