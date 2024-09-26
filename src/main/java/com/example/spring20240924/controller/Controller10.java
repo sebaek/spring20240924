3e02d0cab17a6cd39ecc6e9d05c1453b0f9c1cf8
sebaek <sebaek@gmail.com>
2024-09-26 10:53:01 +0900

    javaBeans, property

--- src/main/java/com/example/spring20240924/controller/Controller10.java
+++ src/main/java/com/example/spring20240924/controller/Controller10.java
@@ -1,10 +1,24 @@
 package com.example.spring20240924.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("main10")
 public class Controller10 {
-    
+
+    @RequestMapping("sub1")
+    public String sub1(Model model) {
+
+        // javaBeans : get, set 메소드가 있는 객체(클래스)
+
+        var obj = new Student();
+        obj.setId(11);
+        obj.setName("donald");
+        obj.setEmail("donald@gmail.com");
+
+        model.addAttribute("student", obj);
+        return null;
+    }
 }
