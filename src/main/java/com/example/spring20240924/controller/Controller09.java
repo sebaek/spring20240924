a5aa86b1572dd897eb3e198584b3571d97efc897
sebaek <sebaek@gmail.com>
2024-09-26 10:27:02 +0900

    Map을 attribute로 사용

--- src/main/java/com/example/spring20240924/controller/Controller09.java
+++ src/main/java/com/example/spring20240924/controller/Controller09.java
@@ -1,14 +1,42 @@
 package com.example.spring20240924.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("main9")
 public class Controller09 {
 
     @RequestMapping("sub1")
-    public String sub1(){
+    public String sub1() {
+        return null;
+    }
+
+    @RequestMapping("sub2")
+    public String sub2(Model model) {
+
+        var map = Map.of(
+                "name", "son",
+                "age", 33,
+                "team", "토트넘");
+        model.addAttribute("attr1", map);
+        model.addAttribute("attr2", List.of("java", "emmet"));
+
+        return null;
+    }
+
+    @RequestMapping("sub3")
+    public String sub3(Model model) {
+        model.addAttribute("person", Map.of(
+                "name", "도널드",
+                "address", "뉴욕",
+                "e mail", "지메일"
+        ));
+
         return null;
     }
 }
