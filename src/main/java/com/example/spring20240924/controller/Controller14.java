439815362133651a2233cf8f17fac2faa5068e5a
sebaek <sebaek@gmail.com>
2024-09-27 15:28:04 +0900

    forEach

--- src/main/java/com/example/spring20240924/controller/Controller14.java
+++ src/main/java/com/example/spring20240924/controller/Controller14.java
@@ -1,6 +1,7 @@
 package com.example.spring20240924.controller;
 
 import com.example.spring20240924.dto.c14.Book;
+import com.example.spring20240924.dto.c14.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,12 @@ public class Controller14 {
                 new Book("react", "adam", "meta", 450, 40000),
                 new Book("vue", "captain", "apple", 300, 35000)));
 
+        model.addAttribute("members", List.of(
+                new Member("흥민", 183, 78, "07/08", "토트넘", "FW"),
+                new Member("강인", 174, 72, "02/19", "파리", "MF"),
+                new Member("현우", 189, 75, "09/25", "울산", "GK"),
+                new Member("희찬", 177, 77, "01/26", "울버햄튼", "MF")
+        ));
     }
 
 }
