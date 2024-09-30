4006544ca4d904df84728348a12313611db11729
sebaek <sebaek@gmail.com>
2024-09-30 16:39:16 +0900

    redirect

---
+++ src/main/java/com/example/spring20240924/controller/Controller17.java
@@ -0,0 +1,17 @@
+package com.example.spring20240924.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("main17")
+public class Controller17 {
+    // Controller16, sub3,sub4,sub5,sub9 가 하는 일 작성
+}
+
+
+
+
+
+
+
