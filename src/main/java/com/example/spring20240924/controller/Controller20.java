f0f9e6336dd89ff7f0717fdc4f8fc4b1b4790b1f
sebaek <sebaek@gmail.com>
2024-10-02 11:50:40 +0900

    post, get

--- src/main/java/com/example/spring20240924/controller/Controller20.java
+++ src/main/java/com/example/spring20240924/controller/Controller20.java
@@ -3,6 +3,9 @@ package com.example.spring20240924.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDate;
+import java.util.Arrays;
+
 @Controller
 @RequestMapping("main20")
 public class Controller20 {
@@ -18,6 +21,16 @@ public class Controller20 {
         System.out.println("name = " + name);
         System.out.println("pw = " + pw);
         System.out.println("note = " + note);
+
+    }
+
+    @RequestMapping("sub3")
+    public void sub3(String[] sel,
+                     LocalDate birth,
+                     Integer age) {
+        System.out.println("Arrays.toString(sel) = " + Arrays.toString(sel));
+        System.out.println("birth = " + birth);
+        System.out.println("age = " + age);
 
     }
 }
