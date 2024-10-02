b4fb4f6ca48f964012d02169ff96357abd8e5636
sebaek <sebaek@gmail.com>
2024-10-02 12:43:35 +0900

    ModelAttribute

---
+++ src/main/java/com/example/spring20240924/controller/Controller21.java
@@ -0,0 +1,39 @@
+package com.example.spring20240924.controller;
+
+import com.example.spring20240924.dto.c21.MyBean;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Controller
+@RequestMapping("main21")
+public class Controller21 {
+    @GetMapping("sub1")
+    public void g1() {
+    }
+
+    @PostMapping("sub1")
+    public void p1(@ModelAttribute(name = "some1") Object o) {
+
+    }
+
+    @GetMapping("sub2")
+    public String g2(@ModelAttribute("some1") Object o) {
+        return "/main21/sub1";
+    }
+
+    // @ModelAttribute 을 붙인 메소드 아규먼트(객체)는
+    // request parameter 가 객체의 property에 binding 된다.
+    @GetMapping("sub3")
+    public void g3() {
+    }
+
+    @PostMapping("sub3")
+    public void p3(@ModelAttribute("some") MyBean b) {
+        System.out.println("b = " + b);
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/view/main21/sub3.jsp
@@ -0,0 +1,14 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form method="post">
+    <input type="text" name="lunch" value="돈까스">
+    <br>
+    <button>전송</button>
+</form>
+</body>
+</html>
