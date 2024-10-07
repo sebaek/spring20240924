b006dbcdc399797b4e8404f45a5fb8b596160f67
sebaek <sebaek@gmail.com>
2024-10-07 11:32:51 +0900

    직원 이름 조회

--- src/main/java/com/example/spring20240924/controller/Controller23.java
+++ src/main/java/com/example/spring20240924/controller/Controller23.java
@@ -1,16 +1,19 @@
 package com.example.spring20240924.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("main23")
 public class Controller23 {
     @GetMapping("sub1")
-    public void sub1() throws SQLException {
+    public void sub1(Model model) throws SQLException {
         String sql = "SELECT FirstName FROM Employees";
 
         String databaseUrl = "jdbc:mariadb://localhost:3306/w3schools";
@@ -28,12 +31,18 @@ public class Controller23 {
         try (con; stmt; rs) {
             // 4. 실행 결과 가공
             // 첫번째 행으로 커서를 이동
-            rs.next();
+//            rs.next();
             // 커서가 가리키는 행의 FirstName 컬럼 데이터 얻기
-            String name1 = rs.getString("FirstName");
-            
-            // Nancy
-            System.out.println("name1 = " + name1);
+//            String name1 = rs.getString("FirstName");
+
+            List<String> names = new ArrayList<>();
+
+            while (rs.next()) {
+                String name = rs.getString("firstName");
+                names.add(name);
+            }
+
+            model.addAttribute("nameList", names);
         }
 
 
