b80423c9dcc8df583810d7e31e31665c07b33340
sebaek <sebaek@gmail.com>
2024-10-08 12:27:55 +0900

    국가별 고객 조회

--- src/main/java/com/example/spring20240924/controller/Controller26.java
+++ src/main/java/com/example/spring20240924/controller/Controller26.java
@@ -1,5 +1,6 @@
 package com.example.spring20240924.controller;
 
+import com.example.spring20240924.dto.c26.Customer;
 import com.example.spring20240924.dto.c26.Category;
 import com.example.spring20240924.dto.c26.Product;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -86,7 +87,58 @@ public class Controller26 {
     }
 
     @GetMapping("sub2")
-    public void sub2() {
-        
+    public void sub2(Model model, String country, String order) throws Exception {
+        String countrySql = """
+                SELECT DISTINCT Country
+                FROM Customers
+                ORDER BY Country
+                """;
+        Connection con = dataSource.getConnection();
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery(countrySql);
+        try (con; stmt; rs) {
+            List<String> countries = new ArrayList<>();
+            while (rs.next()) {
+                countries.add(rs.getString("Country"));
+            }
+            model.addAttribute("countryList", countries);
+        }
+
+        //
+        String customerSql = STR."""
+                SELECT *
+                FROM Customers
+                WHERE Country = '\{country}'
+                """;
+        if (order != null) {
+            customerSql += switch (order) {
+                case "name" -> """
+                        ORDER BY CustomerName
+                        """;
+                case "contact" -> """
+                        ORDER BY ContactName
+                        """;
+                default -> "";
+            };
+        }
+
+        Connection con2 = dataSource.getConnection();
+        Statement stmt2 = con2.createStatement();
+        ResultSet rs2 = stmt2.executeQuery(customerSql);
+        try (con2; stmt2; rs2) {
+            List<Customer> customers = new ArrayList<>();
+            while (rs2.next()) {
+                Customer customer = new Customer();
+                customer.setId(rs2.getString("CustomerID"));
+                customer.setName(rs2.getString("CustomerName"));
+                customer.setContact(rs2.getString("ContactName"));
+                customer.setAddress(rs2.getString("Address"));
+                customer.setCity(rs2.getString("City"));
+                customer.setCountry(rs2.getString("Country"));
+                customer.setPostalCode(rs2.getString("PostalCode"));
+                customers.add(customer);
+            }
+            model.addAttribute("customerList", customers);
+        }
     }
 }
