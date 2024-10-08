8a782a1f73c6d03f07dfcd8cd4e78c687edc98bc
sebaek <sebaek@gmail.com>
2024-10-08 09:29:22 +0900

    비교 연산

--- sql/05operator.sql
+++ sql/05operator.sql
@@ -42,5 +42,16 @@ FROM Customers
 WHERE CustomerName < 'd';
 
 # 1998년 1월 1일(포함, OrderDate) 이후에 주문된 건들 (Orders)
+SELECT *
+FROM Orders
+WHERE OrderDate >= '1998-01-01';
 # 수량(Quantity)이 10개 미만인 주문상세들(OrderDetails)
+SELECT *
+FROM OrderDetails
+WHERE Quantity < 10;
 # 카테고리 번호가 1,2,3 번인 상품들
+SELECT DISTINCT CategoryID
+FROM Products;
+SELECT *
+FROM Products
+WHERE CategoryID < 4;
