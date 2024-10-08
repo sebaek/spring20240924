a060be4084eb30a3167173e08ddcd8b1e679f677
sebaek <sebaek@gmail.com>
2024-10-08 10:36:22 +0900

    상품 조회

---
+++ src/main/java/com/example/spring20240924/controller/Controller26.java
@@ -0,0 +1,76 @@
+package com.example.spring20240924.controller;
+
+import com.example.spring20240924.dto.c26.Category;
+import com.example.spring20240924.dto.c26.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("main26")
+public class Controller26 {
+    @Autowired
+    DataSource dataSource;
+
+    @GetMapping("sub1")
+    public void sub1(Model model, String categoryId) throws Exception {
+        String categorySQL = """
+                SELECT *
+                FROM Categories
+                ORDER BY CategoryID
+                """;
+        Connection con = dataSource.getConnection();
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery(categorySQL);
+        try (con; stmt; rs) {
+            List<Category> categories = new ArrayList<>();
+            while (rs.next()) {
+                Category category = new Category();
+                category.setId(rs.getString("CategoryID"));
+                category.setName(rs.getString("CategoryName"));
+                category.setDescription(rs.getString("Description"));
+
+                categories.add(category);
+            }
+            model.addAttribute("categoryList", categories);
+
+        }
+
+        // 상품 조회 쿼리
+        String productSQL = """
+                SELECT *
+                FROM Products
+                """;
+        if (categoryId != null && !categoryId.equals("0")) {
+            productSQL += STR."""
+                    WHERE CategoryId = \{categoryId}
+                    """;
+        }
+        Connection con2 = dataSource.getConnection();
+        Statement stmt2 = con2.createStatement();
+        ResultSet rs2 = stmt2.executeQuery(productSQL);
+        try (con2; stmt2; rs2) {
+            List<Product> products = new ArrayList<>();
+            while (rs2.next()) {
+                Product product = new Product();
+                product.setId(rs2.getString("ProductID"));
+                product.setName(rs2.getString("ProductName"));
+                product.setCategoryId(rs2.getString("CategoryID"));
+                product.setSupplierId(rs2.getString("SupplierID"));
+                product.setUnit(rs2.getString("Unit"));
+                product.setPrice(rs2.getString("Price"));
+                products.add(product);
+            }
+            model.addAttribute("productList", products);
+        }
+    }
+}
