d484dc7916d99ab671b28f803b3d590c7ae3c167
sebaek <sebaek@gmail.com>
2024-10-08 14:10:33 +0900

    like

---
+++ sql/07like.sql
@@ -0,0 +1,33 @@
+USE w3schools;
+
+# LIKE : ~같은
+SELECT *
+FROM Customers
+WHERE CustomerName = 'Alfreds Futterkiste';
+SELECT *
+FROM Customers
+WHERE CustomerName LIKE '%ed%';
+
+# % : 0개 이상의 임의의 문자들
+SELECT *
+FROM Customers
+WHERE CustomerName LIKE 'A%';
+SELECT *
+FROM Customers
+WHERE CustomerName LIKE '%A';
+SELECT *
+FROM Customers
+WHERE CustomerName LIKE '%A%';
+
+# _ : 임의의 한글자
+SELECT *
+FROM Employees
+WHERE BirthDate LIKE '195_-__-__';
+SELECT *
+FROM Employees
+WHERE BirthDate LIKE '195_-02-__';
+
+# 고객명에 OR 이 포함되어 있는 고객들 조회
+# 고객계약명이  A로 시작하고 O로 끝나는 고객들 조회
+# 생일이 7월인 직원들 조회
+
