a458342be1aad901831812824caeca9cad0d5aa6
sebaek <sebaek@gmail.com>
2024-10-11 11:30:33 +0900

    고객 조회

--- src/main/java/com/example/spring20240924/controller/Controller28.java
+++ src/main/java/com/example/spring20240924/controller/Controller28.java
@@ -1,7 +1,9 @@
 package com.example.spring20240924.controller;
 
+import com.example.spring20240924.dto.c26.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 @Controller
@@ -86,6 +89,31 @@ public class Controller28 {
     }
 
     @GetMapping("sub5")
-    public void sub5() {
+    public void sub5(String id, Model model) {
+        String sql = """
+                SELECT * FROM Customers
+                WHERE CustomerId = ?
+                """;
+        try {
+            Connection con = dataSource.getConnection();
+            PreparedStatement pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, id);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                Customer customer = new Customer();
+                customer.setId(rs.getString("CustomerId"));
+                customer.setName(rs.getString("CustomerName"));
+                customer.setContact(rs.getString("ContactName"));
+                customer.setAddress(rs.getString("Address"));
+                customer.setCity(rs.getString("City"));
+                customer.setCountry(rs.getString("Country"));
+                customer.setPostalCode(rs.getString("PostalCode"));
+                model.addAttribute("customer", customer);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
