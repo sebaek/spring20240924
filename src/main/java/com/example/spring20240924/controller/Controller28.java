1bce0148342b2af0e4461d7ac5be233e198842b8
sebaek <sebaek@gmail.com>
2024-10-11 10:48:07 +0900

    insert

--- src/main/java/com/example/spring20240924/controller/Controller28.java
+++ src/main/java/com/example/spring20240924/controller/Controller28.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -47,5 +48,40 @@ public class Controller28 {
     }
 
     // get /main28/sub3 은 상품입력 form 이 있는 jsp로 forward
-    // post /main28/sub4 은 새 상품을 insert하는 로직 실
+    @GetMapping("sub3")
+    public void sub3() {
+
+    }
+
+    // post /main28/sub4 은 새 상품을 insert하는 로직 실행
+    @PostMapping("sub4")
+    public String sub4(String name,
+                       String unit,
+                       String price,
+                       RedirectAttributes rttr) {
+        String sql = """
+                INSERT INTO Products
+                    (ProductName, Unit, Price)
+                VALUES
+                    (?, ?, ?)
+                """;
+
+        try {
+            Connection conn = dataSource.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            try (conn; pstmt) {
+                pstmt.setString(1, name);
+                pstmt.setString(2, unit);
+                pstmt.setString(3, price);
+                int count = pstmt.executeUpdate();
+                if (count == 1) {
+                    rttr.addFlashAttribute("message", "새 상품이 등록되었습니다.");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return "redirect:/main28/sub3";
+    }
 }
