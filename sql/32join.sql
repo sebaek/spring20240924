433d36507f5ff4fb21686003105ae8e2be51b285
sebaek <sebaek@gmail.com>
2024-10-16 10:14:39 +0900

    join

--- sql/32join.sql
+++ sql/32join.sql
@@ -17,3 +17,25 @@ FROM Orders o
 WHERE OrderDate = '1996-07-04';
 
 # 1996년 7월 4일에 주문된 상품과 카테고리 정보 조회
+SELECT o.OrderDate, p.ProductName, c.CategoryName
+FROM Orders o
+         JOIN OrderDetails od
+              ON o.OrderID = od.OrderID
+         JOIN Products p
+              ON p.ProductID = od.ProductID
+         JOIN Categories c
+              ON p.CategoryID = c.CategoryID
+WHERE o.OrderDate = '1996-07-04';
+
+# 5번 직원이 처리한 주문의 주문날짜와 상품들 조회
+SELECT e.FirstName, e.LastName, o.OrderDate, p.ProductName, od.Quantity
+FROM Orders o
+         JOIN OrderDetails od
+              ON o.OrderID = od.OrderID
+         JOIN Products p
+              ON od.ProductID = p.ProductID
+         JOIN Employees e
+              ON o.EmployeeID = e.EmployeeID
+WHERE e.EmployeeID = 5
+ORDER BY o.OrderDate DESC;
+
