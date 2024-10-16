c06539677a84bada378a35d77cf3beced9132d01
sebaek <sebaek@gmail.com>
2024-10-16 16:26:24 +0900

    연산

---
+++ sql/38operator.sql
@@ -0,0 +1,49 @@
+SELECT 3;
+SELECT 3 + 5;
+SELECT 3 * 5;
+SELECT 3 / 5;
+SELECT 5 % 3;
+# SELECT 'hello' || 'world';
+SELECT CONCAT('hello', 'world');
+
+USE w3schools;
+SELECT LastName, FirstName
+FROM Employees;
+
+SELECT CONCAT(FirstName, ' ', LastName) full_name
+FROM Employees;
+
+SELECT CONCAT(Address, ', ', City, ', ', Country) full_address
+FROM Customers;
+
+# 1번 고객의 총 주문 금액
+SELECT c.CustomerName,
+       o.OrderDate,
+       p.ProductName,
+       od.Quantity,
+       p.Price,
+       (p.Price * od.Quantity) 총액
+FROM Customers c
+         JOIN Orders o
+              ON c.CustomerID = o.CustomerID
+         JOIN OrderDetails od
+              ON o.OrderID = od.OrderID
+         JOIN Products p
+              ON od.ProductID = p.ProductID
+WHERE c.CustomerID = 1;
+
+# 고객별 총소비금액
+SELECT c.CustomerName,
+       SUM((p.Price * od.Quantity)) 총소비금액
+FROM Customers c
+         JOIN Orders o
+              ON c.CustomerID = o.CustomerID
+         JOIN OrderDetails od
+              ON o.OrderID = od.OrderID
+         JOIN Products p
+              ON od.ProductID = p.ProductID
+GROUP BY c.CustomerID;
+
+# 1998년 3월에 가장 많은 매출을 올린 직원 조회
+
+
