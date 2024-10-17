8891e6e1e8543929225fc2adcafd2effc02a798d
sebaek <sebaek@gmail.com>
2024-10-17 09:32:43 +0900

    commit

---
+++ src/main/java/com/example/spring20240924/controller/Controller30.java
@@ -0,0 +1,10 @@
+package com.example.spring20240924.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("main30")
+public class Controller30 {
+    
+}
