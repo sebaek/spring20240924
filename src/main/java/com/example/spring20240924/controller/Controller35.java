2e47056ba64be477499f02e15b6051ec1cdd6da4
sebaek <sebaek@gmail.com>
2024-10-18 12:25:06 +0900

    snake_case -> camelCase

---
+++ src/main/java/com/example/spring20240924/controller/Controller35.java
@@ -0,0 +1,22 @@
+package com.example.spring20240924.controller;
+
+import com.example.spring20240924.mapper.Mapper05;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("main35")
+public class Controller35 {
+
+    private final Mapper05 mapper05;
+
+    @GetMapping("sub1")
+    public void sub1() {
+
+    }
+
+}
---
+++ src/main/java/com/example/spring20240924/mapper/Mapper05.java
@@ -0,0 +1,7 @@
+package com.example.spring20240924.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface Mapper05 {
+}
