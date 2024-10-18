beb19e658d4a81d6d1a342be30439579441e67bb
sebaek <sebaek@gmail.com>
2024-10-18 15:15:44 +0900

    delete

--- src/main/java/com/example/spring20240924/controller/Controller37.java
+++ src/main/java/com/example/spring20240924/controller/Controller37.java
@@ -1,6 +1,7 @@
 package com.example.spring20240924.controller;
 
 import com.example.spring20240924.mapper.Mapper07;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +15,14 @@ public class Controller37 {
 
     @GetMapping("sub1")
     public void sub1(Integer id) {
-        mapper07.delete1(id);
+        int cnt = mapper07.delete1(id);
+        System.out.println(cnt + "개 행 삭제됨");
     }
+
+    // /main37/sub2?id=5
+//    @GetMapping("sub2")
+    // 요청 파라미터 id 값에 해당하는 상품 삭제하는
+    // 2번째 메소드, mapper에 delete2 메소드 작성
+    
 
 }
--- src/main/java/com/example/spring20240924/mapper/Mapper07.java
+++ src/main/java/com/example/spring20240924/mapper/Mapper07.java
@@ -20,5 +20,5 @@ public interface Mapper07 {
             DELETE FROM Customers
             WHERE CustomerId = #{id}
             """)
-    void delete1(Integer id);
+    int delete1(Integer id);
 }
