1aebeaff25e624013762e5fec14e71d63e7a7e39
sebaek <sebaek@gmail.com>
2024-10-18 15:27:05 +0900

    데이터 삽입

--- src/main/java/com/example/spring20240924/controller/Controller37.java
+++ src/main/java/com/example/spring20240924/controller/Controller37.java
@@ -20,9 +20,30 @@ public class Controller37 {
     }
 
     // /main37/sub2?id=5
-//    @GetMapping("sub2")
     // 요청 파라미터 id 값에 해당하는 상품 삭제하는
     // 2번째 메소드, mapper에 delete2 메소드 작성
-    
+    @GetMapping("sub2")
+    public void sub2(Integer id) {
+        int cnt = mapper07.delete2(id);
+        System.out.println(cnt + "개 상품 데이터 삭제됨");
+    }
+
+    // /main37/sub3?name=lee&city=paris&country=france
+    @GetMapping("sub3")
+    public void sub3(String name,
+                     String city,
+                     String country) {
+
+//        name = "son";
+//        city = "seoul";
+//        country = "korea";
+
+        int cnt = mapper07.insert1(name, city, country);
+        System.out.println(cnt + "명 고객 정보 입력됨");
+    }
+
+    // 새 상품(상품명, 가격)을 입력하는 코드 작성
+    // 4번째 메소드, mapper.insert2 메소
+
 
 }
--- src/main/java/com/example/spring20240924/mapper/Mapper07.java
+++ src/main/java/com/example/spring20240924/mapper/Mapper07.java
@@ -1,6 +1,7 @@
 package com.example.spring20240924.mapper;
 
 import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -21,4 +22,17 @@ public interface Mapper07 {
             WHERE CustomerId = #{id}
             """)
     int delete1(Integer id);
+
+    @Delete("""
+            DELETE FROM Products
+            WHERE ProductId = #{id}
+            """)
+    int delete2(Integer id);
+
+    @Insert("""
+            INSERT INTO Customers
+            (CustomerName, City, Country)
+            VALUES (#{name}, #{city}, #{country})
+            """)
+    int insert1(String name, String city, String country);
 }
