3606cb3a827e7a42ac1d25cdaafe0930df633f24
sebaek <sebaek@gmail.com>
2024-10-21 12:24:01 +0900

    transactional, Service, 3 tier architecture, checked exception, runtime exception

--- src/main/java/com/example/spring20240924/controller/Controller39.java
+++ src/main/java/com/example/spring20240924/controller/Controller39.java
@@ -1,13 +1,10 @@
 package com.example.spring20240924.controller;
 
-import com.example.spring20240924.mapper.Mapper09;
 import com.example.spring20240924.service.Service01;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Map;
 
 @Controller
 @RequestMapping("main39")
@@ -17,6 +14,21 @@ public class Controller39 {
 
     @GetMapping("sub1")
     public void method1(Integer money) {
-        service01.transferMoney(money);
+        service01.transferMoney1(money);
+    }
+
+    @GetMapping("sub2")
+    public void method2(Integer money) {
+        service01.transferMoney2(money); // runtime exception
+    }
+
+    @GetMapping("sub3")
+    public void method3(Integer money) throws ClassNotFoundException {
+        service01.transferMoney3(money); // checked exception
+    }
+
+    @GetMapping("sub4")
+    public void method4(Integer money) {
+        service01.transferMoney4(money);
     }
 }
--- src/main/java/com/example/spring20240924/service/Service01.java
+++ src/main/java/com/example/spring20240924/service/Service01.java
@@ -8,12 +8,35 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class Service01 {
     private final Mapper09 mapper09;
 
-    @Transactional
-    public void transferMoney(Integer money) {
+    public void transferMoney1(Integer money) {
         mapper09.updateMoney1(money);
+        mapper09.updateMoney2(money);
+    }
+
+    public void transferMoney2(Integer money) {
+        mapper09.updateMoney1(money);
+        int a = 0;
+        int b = 5 / a;
+        mapper09.updateMoney2(money);
+    }
+
+    public void transferMoney3(Integer money) throws ClassNotFoundException {
+        mapper09.updateMoney1(money);
+        Class.forName("j.l.String");
+        mapper09.updateMoney2(money);
+    }
+
+    public void transferMoney4(Integer money) {
+        mapper09.updateMoney1(money);
+        try {
+            Class.forName("j.l.String");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
         mapper09.updateMoney2(money);
     }
 }
