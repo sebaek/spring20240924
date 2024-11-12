49268965cdfedc0edd6a5572d55925d44a2e3e0d
sebaek <sebaek@gmail.com>
2024-11-12 10:19:48 +0900

    json을 javaBean(dto)로 받기

--- src/main/java/com/example/spring20240924/api/ApiController2.java
+++ src/main/java/com/example/spring20240924/api/ApiController2.java
@@ -2,7 +2,6 @@ package com.example.spring20240924.api;
 
 import com.example.spring20240924.dto.a2.Book;
 import com.example.spring20240924.dto.a2.Person;
-
 import com.example.spring20240924.dto.a2.Player;
 import com.example.spring20240924.dto.a2.Product;
 import org.springframework.stereotype.Controller;
@@ -10,11 +9,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Map;
+import java.util.*;
 
 @Controller
 @RequestMapping("/api/main2")
 public class ApiController2 {
+    @PostMapping("sub6")
+    public void method6(@RequestBody List<Person> people) {
+        System.out.println(people);
+    }
+
+    @PostMapping("sub5")
+    public void method5(@RequestBody List<String> items) {
+        System.out.println(items);
+    }
+
     @PostMapping("sub4")
     public void method4(@RequestBody Product product) {
         System.out.println(product);
