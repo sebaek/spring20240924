ed460c27939e06e3fe1ce911616d6784f6c25e12
sebaek <sebaek@gmail.com>
2024-11-12 15:40:59 +0900

    고객조회, 공급자조회

--- src/main/java/com/example/spring20240924/api/ApiController6.java
+++ src/main/java/com/example/spring20240924/api/ApiController6.java
@@ -1,6 +1,7 @@
 package com.example.spring20240924.api;
 
 import com.example.spring20240924.dto.a6.Customer;
+import com.example.spring20240924.dto.a6.Supplier;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +13,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/api/main6")
 @RequiredArgsConstructor
 public class ApiController6 {
-    private final CustomerService service;
+    private final Service6 service;
+
+    @GetMapping("sub2/{supplierId}")
+    public Supplier getSupplier(@PathVariable Integer supplierId) {
+        return service.getSupplier(supplierId);
+    }
 
     @GetMapping("sub1/{customerId}")
     @ResponseBody
---
+++ src/main/java/com/example/spring20240924/api/Mapper6.java
@@ -0,0 +1,36 @@
+package com.example.spring20240924.api;
+
+import com.example.spring20240924.dto.a6.Customer;
+import com.example.spring20240924.dto.a6.Supplier;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface Mapper6 {
+    @Select("""
+            SELECT CustomerID id,
+                   CustomerName name,
+                   ContactName contact,
+                   Address,
+                   City,
+                   PostalCode,
+                   Country
+            FROM Customers
+            WHERE CustomerID=#{customerID}
+            """)
+    Customer selectCustomerById(Integer customerId);
+
+    @Select("""
+            SELECT SupplierId id,
+                   SupplierName name,
+                   ContactName contact,
+                   Address,
+                   City,
+                   PostalCode,
+                   Country,
+                   Phone
+            FROM Suppliers
+            WHERE SupplierID=#{supplierID}
+            """)
+    Supplier selectSupplierById(Integer supplierId);
+}
