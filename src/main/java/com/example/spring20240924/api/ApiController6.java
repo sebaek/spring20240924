b565e46a91f283c049f80f0405037392818be415
sebaek <sebaek@gmail.com>
2024-11-12 16:26:10 +0900

    RestController

--- src/main/java/com/example/spring20240924/api/ApiController6.java
+++ src/main/java/com/example/spring20240924/api/ApiController6.java
@@ -4,25 +4,22 @@ import com.example.spring20240924.dto.a6.Customer;
 import com.example.spring20240924.dto.a6.Supplier;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+//@Controller
+//@ResponseBody
+@RestController
 @RequestMapping("/api/main6")
 @RequiredArgsConstructor
 public class ApiController6 {
     private final Service6 service;
 
     @GetMapping("sub2/{supplierId}")
-    @ResponseBody
     public Supplier getSupplier(@PathVariable Integer supplierId) {
         return service.getSupplier(supplierId);
     }
 
     @GetMapping("sub1/{customerId}")
-    @ResponseBody
     public Customer get(@PathVariable Integer customerId) {
         return service.getCustomer(customerId);
     }
