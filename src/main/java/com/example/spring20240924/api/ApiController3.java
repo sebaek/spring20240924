5e72fb2b0ae29e0c5dd4e7d34547db4b43039256
sebaek <sebaek@gmail.com>
2024-11-12 10:45:41 +0900

    json을 javaBean(dto)로 받기

---
+++ src/main/java/com/example/spring20240924/api/ApiController3.java
@@ -0,0 +1,42 @@
+package com.example.spring20240924.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping("/api/main3")
+public class ApiController3 {
+
+    @DeleteMapping("sub6")
+    public void sub6(@RequestBody Map<String, Object> map) {
+        System.out.println(map);
+    }
+
+    @DeleteMapping("sub5")
+    public void sub5() {
+        System.out.println("ApiController3.sub5");
+    }
+
+    @PutMapping("sub4")
+    public void sub4(@RequestBody Map<String, Object> map) {
+        System.out.println(map);
+
+    }
+
+    @PutMapping("sub3")
+    public void sub3() {
+        System.out.println("ApiController3.sub3");
+    }
+
+    @PostMapping("sub2")
+    public void sub2(@RequestBody Map<String, Object> map) {
+        System.out.println(map);
+    }
+
+    @GetMapping("sub1")
+    public void sub1() {
+        System.out.println("ApiController3.sub1");
+    }
+}
